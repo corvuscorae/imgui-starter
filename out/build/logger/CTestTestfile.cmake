f945d5002a230f01695e70c4035c9cea770b459f
Raven Ruiz <102258919+corvuscorae@users.noreply.github.com>
2026-01-16 11:42:55 -0800

    Delete out/build/logger directory

--- out/build/logger/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/raven/OneDrive/Desktop/.123/imgui-starter
-# Build directory: C:/Users/raven/OneDrive/Desktop/.123/imgui-starter/out/build/logger
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
